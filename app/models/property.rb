a976edc0ec17fab37d8b5565ac80b90672fd9641
Jacob <jacobphelan@hotmail.com>
2024-07-08 15:35:13 +1000

    add: Property can build_from_page_data

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,17 @@
 class Property < ApplicationRecord
 	belongs_to :suburb
+
+	def self.build_from_page_data(data)
+		data.each do |prop|
+			self.create!(
+				link: prop[0],
+				address: prop[1]['address'],
+				beds: prop[1]['beds'],
+				baths: prop[1]['baths'],
+				cars: prop[1]['cars'],
+				land: prop[1]['land'],
+				price: prop[1]['price']
+			)
+		end
+	end
 end
--- app/models/suburb.rb
+++ app/models/suburb.rb
@@ -4,17 +4,7 @@ class Suburb < ApplicationRecord
 	def setup_search(domain_suburb)
 		scrape = Scraper.new
 		property_data = scrape.process_request(domain_suburb)
-		property_data.each do |prop|
-			properties.create!(
-				link: prop[0],
-				address: prop[1]['address'],
-				beds: prop[1]['beds'],
-				baths: prop[1]['baths'],
-				cars: prop[1]['cars'],
-				land: prop[1]['land'],
-				price: prop[1]['price']
-			)
-		end
+		properties.build_from_page_data(property_data)
 	end
 
 end
